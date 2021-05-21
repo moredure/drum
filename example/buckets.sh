33827df8dcee0b2992181674555e7b44f44517ce
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-05-21 18:17:23 +0300

    Update buckets.sh

--- example/buckets.sh
+++ example/buckets.sh
@@ -2,12 +2,13 @@
 
 rm -rf /tmp/database
 
-rm /tmp/buckets/0_bucket.kv
-rm /tmp/buckets/1_bucket.kv
-rm /tmp/buckets/0_bucket.aux
-rm /tmp/buckets/1_bucket.aux
-
-dd if=/dev/zero of=/tmp/buckets/0_bucket.kv bs=1024 count=1
-dd if=/dev/zero of=/tmp/buckets/1_bucket.kv bs=1024 count=1
-dd if=/dev/zero of=/tmp/buckets/0_bucket.aux bs=1024 count=1
-dd if=/dev/zero of=/tmp/buckets/1_bucket.aux bs=1024 count=1
+# 4 buckets 0..3 each for 1024 * 2048 bytes
+for idx in $(seq 0 7)
+do
+  touch /tmp/buckets/${idx}_bucket.kv
+  touch /tmp/buckets/${idx}_bucket.aux
+  rm /tmp/buckets/${idx}_bucket.kv
+  rm /tmp/buckets/${idx}_bucket.aux
+  dd if=/dev/zero of=/tmp/buckets/${idx}_bucket.kv bs=1024 count=1024
+  dd if=/dev/zero of=/tmp/buckets/${idx}_bucket.aux bs=1024 count=1024
+done
