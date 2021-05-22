0f7d8e572db577747209ff70829b7305a1d5c0ba
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2021-05-22 17:41:03 +0300

    Update dispatcher.go

--- dispatcher.go
+++ dispatcher.go
@@ -7,25 +7,3 @@ type Dispatcher interface {
 	DuplicateKeyUpdateEvent(*DuplicateKeyUpdateEvent)
 	UpdateEvent(*UpdateEvent)
 }
-
-type DispatcherFunc func(interface{})
-
-func (d DispatcherFunc) UniqueKeyCheckEvent(event *UniqueKeyCheckEvent) {
-	d(event)
-}
-
-func (d DispatcherFunc) DuplicateKeyCheckEvent(event *DuplicateKeyCheckEvent) {
-	d(event)
-}
-
-func (d DispatcherFunc) UniqueKeyUpdateEvent(event *UniqueKeyUpdateEvent) {
-	d(event)
-}
-
-func (d DispatcherFunc) DuplicateKeyUpdateEvent(event *DuplicateKeyUpdateEvent) {
-	d(event)
-}
-
-func (d DispatcherFunc) UpdateEvent(event *UpdateEvent) {
-	d(event)
-}
